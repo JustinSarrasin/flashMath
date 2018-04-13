5289f8f84b75ea74c1303ead0db36a9b5e5c70dc
Justin Sarrasin <sarrasin.justin@gmail.com>
2018-04-12 23:41:03 -0400

    single click firing

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -39,54 +39,72 @@ const results = [
       {
          question: "10+2=12",
          answer: true
+      },
+      {
+         question: "2+2=4",
+         answer: true
+      },
+      {
+         question: "10+4=14",
+         answer: true
+      },
+      {
+         question: "8+2=9",
+         answer: false
+      },
+      {
+         question: "2+1=4",
+         answer: false
+      },
+      {
+         question: "3+3=4",
+         answer: false
+      },
+      {
+         question: "4+2=6",
+         answer: true
+      },
+      {
+         question: "10+3=7",
+         answer: false
+      },
+      {
+         question: "10+2=12",
+         answer: true
+      },
+      {
+         question: "2+2=4",
+         answer: true
+      },
+      {
+         question: "10+4=14",
+         answer: true
+      },
+      {
+         question: "8+2=9",
+         answer: false
+      },
+      {
+         question: "2+1=4",
+         answer: false
+      },
+      {
+         question: "3+3=4",
+         answer: false
+      },
+      {
+         question: "4+2=6",
+         answer: true
+      },
+      {
+         question: "10+3=7",
+         answer: false
+      },
+      {
+         question: "10+2=12",
+         answer: true
       }
    ] 
-
-let newArray = [];
-for(i = 0; i < results.length; i++){
-   newArray.push((results[Math.floor(Math.random() * results.length)]));
-}
-// console.log(newArray[0].question)
-
-// let rand = results[Math.floor(Math.random() * results.length)];
-
-newArray.forEach(function(index, i){
-   // console.log(index, i)
-   // console.log(newArray[0].question)
-})
-
-// newArray.map((item, i) => {
-//    // console.log(index, i)
-//    // console.log(item.question);
-//    newArray = newArray
-//       .map(question => item.question)
-//       .map(answer => item.answer)
-//    // console.log(index, i)
-//    console.log(item.answer, item.question);
-// })
-
-
-
-// console.log(newArray[i]);
-
-// console.log(newArray.question)
-// console.log(newArray[0].answer)
-
-// console.log(index)
-
-// console.log(rand)
-
-// let reformattedArray = results.map(obj => {
-//    let rObj = {};
-//    rObj[obj.key] = obj.value;
-//    return rObj;
-//    console.log(results);
-// })
-
-
-// console.log(rand.question, rand.answer);
-// console.log(rand);
-
 
 // starting home page
 freakMath.start = () => {
@@ -102,151 +120,107 @@ freakMath.hideQuestionPage = () => {
 
 }
 
+// freakMath.getInfo = () => {
+//    // freakMath.getQuestion(results[freakMath.counter].question);
+//    // freakMath.getAnswer(results[freakMath.counter].answer)
+//    // freakMath.questionPage();
+// }
+
 // question page
 freakMath.questionPage = () => {
-   $('.correct').on('click', function () {
-      
-      freakMath.counter = freakMath.counter + 1;
-
-      freakMath.getQuestion(newArray[freakMath.counter].question);
-      freakMath.getAnswer(newArray[freakMath.counter].result)
-
-      console.log(newArray[freakMath.counter].question)
-      console.log(newArray[freakMath.counter].answer)
-      console.log([freakMath.counter])
-   })
-
+   $('.start').on('click', function (e) {
+      e.preventDefault();
    freakMath.counter = freakMath.counter + 1;
 
-   freakMath.getQuestion(newArray[freakMath.counter].question);
-   freakMath.getAnswer(newArray[freakMath.counter].result)
-
-   console.log(newArray[freakMath.counter].question)
-   console.log(newArray[freakMath.counter].answer)
-   console.log([freakMath.counter])
-}
-
-// get new question
-freakMath.getQuestion = (question) => {
-
-   // freakMath.counter = freakMath.counter + 1;
-   console.log(freakMath.counter);
-   $('.question').empty();
-   $('.question').append(question);
-   console.log(question)
-   // $('.question').empty();
-   // $('.question').append(question);
-
-   // $('.correct').on('click', function (){
-   //    freakMath.question = (question) => {
-   //       $('.question').empty();
-   //       // $('.question').append(question);
-         
-   //    }
-   // })
-   
-}
-
-freakMath.getAnswer = () => {
-
-}
-
-
-
-// freakMath.showQuestion = () => {
-//    $('.question').append(`<h3>` + questions.results.question + `<h3>`);
-// }
-
-// freakMath.showQuestion = () => {
-//    let newArray = [];
-//    // for (i = 0; i < results.length; i++) {
-//    //    newArray = results[Math.floor(Math.random() * results.length)];
-//    //    console.log(newArray)
-//       console.log(results)
-//       // console.log(results[Math.floor(Math.random() * results.length)]);
-//       // return results[Math.floor(Math.random() * results.length)]
-//    $('.question').append(`<h3>` + results[Math.floor(Math.random() * results.length)].question + `<h3>`);
-//    // }
-   
-
-
-//    // freakMath.correct(rand.question);
-
-// }
-freakMath.showQuestion = () => {
-   // let newArray = [];
-   // for (i = 0; i < results.length; i++) {
-   //    newArray = results[Math.floor(Math.random() * results.length)];
-   //    console.log(newArray)
-      // console.log(results)
-      // newArray.map((item, i) => {
-      // // console.log(index, i)
-      // // console.log(item.question);
-      // newArray = newArray
-      //    .map(question => item.question)
-      //    .map(answer => item.answer)
-      // console.log(index, i)
-      // console.log(item.answer, item.question);
-      // $('.question').append(`<h3>` + item.question + `<h3>`);
-         // $('.question').empty();
-         // $('.question').append(`<h3>` + newArray[0].question + `<h3>`);
-         // $('.correct').on('click', function () {
-         //    $('.question').empty();
-         //    $('.question').append(`<h3>` + newArray[1].question + `<h3>`);   
-         // })
-   
-      // console.log(results[Math.floor(Math.random() * results.length)]);
-      // return results[Math.floor(Math.random() * results.length)]
-   
-   
-
-   // let reformattedArray = results.map(obj => {
-   //    let rObj = {};
-   //    rObj[obj.key] = obj.value;
-   //    return rObj;
-   //    console.log(results);
-   // })
-
-   // freakMath.correct(rand.question);
-
-}
-
-freakMath.correct = (correct) => {
-   $('.correct').on('click', function() {
-      if (newArray.question === true){
-         console.log('correct');
-      } else {
-         console.log('wrong');
-      }
-      // $('.question').empty();
-      // $('.question').append(`<h3>` + rand.question + `<h3>`);
-      })
-      // console.log(correct);
-   
-}
-// freakMath.correct = (correct) => {
-//    $('.correct').on('click', function() {
-//       if (results[Math.floor(Math.random() * results.length)].answer === true){
-//          console.log('correct');
-//       } else {
-//          console.log('wrong');
-//       }
-//       $('.question').empty();
-//       $('.question').append(`<h3>` + results[Math.floor(Math.random() * results.length)].question + `<h3>`);
-//       })
-//       console.log(correct);
-   
-// }
-
-freakMath.wrong = (wrong) => {
-   $('.wrong').on('click', function() {
-      if(question === false) {
-         console.log('correct');
-      } else {
-         console.log('wrong');
-      }
+   freakMath.getQuestion(results[freakMath.counter].question);
+   freakMath.getAnswer(results[freakMath.counter].answer)
    })
 }
+
+// puts question to page
+freakMath.getQuestion = (question) => {
+   $('.question').empty();
+   $('.question').append(question);
+   console.log(question);
+   // freakMath.counter;
+   console.log(freakMath.counter);
+}
+
+// const currentQuestion = (results[freakMath.counter].question);
+
+// const currentAnswer = (results[freakMath.counter].answer);
+
+
+freakMath.getAnswer = (answer) => {
+   // let clicked = false;
+   // function clickHandler(){
+   //    clicked = true;
+   // }
+   
+   
+   $('.correct').off().on('click', function (e) {
+      e.preventDefault();
+      // clicked = true;
+      // console.log(clicked)
+      let currentQuestion = (results[freakMath.counter].question);
+      // console.log(currentQuestion)
+
+      let currentAnswer = (results[freakMath.counter].answer);
+
+      if (currentAnswer === true) {
+         // console.log('correct');
+         console.log('giddyup')
+      } else {
+         console.log('nope');
+      }
+      
+
+      console.log('clicked');
+   })
+
+   $('.wrong').off().on('click', function (e) {
+      e.preventDefault();
+      clicked = true;
+      // console.log(clicked)
+
+      let currentQuestion = (results[freakMath.counter].question);
+
+      let currentAnswer = (results[freakMath.counter].answer);
+      
+      
+      if (currentAnswer === false) {
+         freakMath.addScore();
+         console.log('giddyuasdfp')
+      } else {
+         alert('nope');
+      }
+
+      console.log('clicked');
+   })
+   console.log(answer)  
+}
+
+// freakMath.nextQuestion = () => {
+
+// }
+
+
+
+
+// freakMath.correct = (question) => {
+
+// } 
+
+
+// freakMath.wrong = (wrong) => {
+//    // $('.wrong').on('click', function() {
+//    //    if( === false) {
+//    //       console.log('correct');
+//    //    } else {
+//    //       console.log('wrong');
+//    //    }
+//    // })
+// }
 
 // question timer
 freakMath.timer = () => {
@@ -256,7 +230,8 @@ freakMath.timer = () => {
 
 // add score for every correct answer
 freakMath.addScore = () => {
-
+   freakMath.score++;
+   // console.log(freakMath.score);
 }
 
 // toggle sound option on question page 
@@ -273,16 +248,21 @@ freakMath.toggleSound = () => {
 freakMath.init = () => {
    freakMath.start();
    freakMath.questionPage();
+   // freakMath.getInfo();
    freakMath.hideQuestionPage();
+   // freakMath.nextQuestion();
    // freakMath.getQuestion();
    // freakMath.getAnswer();
-   freakMath.showQuestion();
+   // freakMath.showQuestion();
    // freakMath.queueNextQuestion();
-   freakMath.correct();
-   freakMath.wrong();
+   // freakMath.correct();
+   freakMath.addScore();
+   // freakMath.wrong();
 }
 
 // Document ready function
 $(function () {
    freakMath.init();
 });
+
+
